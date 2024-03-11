793bb21080b5991ca14d209c9b41609aaf4ac6fb
wpgur <wpgur0510@naver.com>
2024-03-11 16:20:30 +0900

    twitter module

--- module/twitter_module.py
+++ module/twitter_module.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time, re
+from config import Twitter_ID, Twitter_PW
+
 
 twitter_module = Blueprint("twitter_module", __name__)
 
@@ -17,7 +19,36 @@ def twitter_result():
             self.driver_path = driver_path
             self.username = username
 
-        def scrape_twitter_profile(self):
+        def login_twitter(self, driver, target_url, login_name, login_pw):
+            twitter_url = 'https://twitter.com/i/flow/login'
+            driver.implicitly_wait(10)
+            driver.get(twitter_url)
+            print('트위터 로그인')
+            time.sleep(3)
+
+            username_input = driver.find_element(By.CSS_SELECTOR, "input[name='text']")
+            username_input.send_keys(login_name)
+            next_button = driver.find_element(By.XPATH, '//div[@class="css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-ywje51 r-usiww2 r-13qz1uu r-2yi16 r-1qi8awa r-ymttw5 r-1loqt21 r-o7ynqc r-6416eg r-1ny4l3l"]')
+            print("next_button", next_button)
+            next_button.click()
+            print('next 클릭 완료')
+
+            time.sleep(2)
+
+            
+            password_input = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
+            password_input.send_keys(login_pw)
+            login_button = driver.find_element(By.XPATH, "//div[@class='css-175oi2r r-sdzlij r-1phboty r-rs99b7 r-lrvibr r-19yznuf r-64el8z r-1dye5f7 r-o7ynqc r-6416eg r-icoktb r-1ny4l3l'][3]")
+
+            login_button.click()
+            print('login 클릭 완료')
+
+            time.sleep(3)
+            driver.get(target_url)
+            time.sleep(3)
+            print('트위터 진입성공')
+
+        def scrape_twitter_profile(self, login_name, login_pw):
             try:
                 url = 'https://twitter.com/' + self.username
                 options = webdriver.ChromeOptions()
@@ -27,7 +58,18 @@ def twitter_result():
                 options.add_argument('--no-sandbox')
                 driver = webdriver.Chrome(executable_path=self.driver_path, options=options)
                 driver.get(url)
+
                 time.sleep(5)
+                self.login_twitter(driver, url, login_name, login_pw)
+                time.sleep(5)
+
+                filename = url[url.find('//')+2:]
+                filename = filename.replace('/','_')
+                f = open(filename+'.html','w', encoding='utf-8')
+                f.write(str(driver.page_source))
+                f.close()
+
+
                 try:                
                     name = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div:nth-child(1) > div > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci > div > h2 > div > div > div > div > span.css-901oao.css-16my406.r-1awozwy.r-18jsvk2.r-6koalj.r-poiln3.r-b88u0q.r-bcqeeo.r-1udh08x.r-3s2u2q.r-qvutc0 > span > span:nth-child(1)')
                     name = name.text
@@ -87,7 +129,7 @@ def twitter_result():
     find_name = request.cookies.get("NAME")
 
     scraper = SNSProfileScraper(find_name , driver_path)
-    twitter_profile = scraper.scrape_twitter_profile()
+    twitter_profile = scraper.scrape_twitter_profile(Twitter_ID, Twitter_PW)
 
     result ={}
 
